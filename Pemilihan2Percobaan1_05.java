77cc7be849aa0e732768a8ebf21ed33bac3055c5
Marcel-12345 <anselmus.marcel@gmail.com>
2024-10-03 07:54:32 +0700

    commit tugas

---
+++ Pemilihan2Percobaan1_05.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan1_05{
+    public static void main(String[] args){
+
+        Scanner input05 = new Scanner(System.in);
+
+        int tahun;
+
+        System.out.print("Masukkan tahun: ");
+        tahun = input05.nextInt();
+
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100) != 0)
+            System.out.print("Tahun kabisat");
+
+        } else 
+        System.out.print("Bukan tahun kabisat");
+                
+    }
+}
